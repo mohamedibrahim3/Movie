fa475309551a964a8935a909e0e5aa2a3760063f
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2024-12-29 13:49:40 +0200

    Add Items in Home screen

---
+++ app/src/main/java/com/mada/movieapp/item/ItemUi.kt
@@ -0,0 +1,94 @@
+package com.mada.movieapp.item
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.mada.movieapp.domain.data.Data
+
+@Composable
+fun ItemUi(itemIndex: Int, movieList: List<Data>, navController: NavController){
+
+    Card(
+        Modifier
+            .wrapContentSize()
+            .padding(10.dp)
+            .clickable {
+                navController.navigate("Details screen/${movieList[itemIndex].id}")
+            },
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            AsyncImage(
+                model = movieList[itemIndex].poster,
+                contentDescription = movieList[itemIndex].title,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(Color.LightGray.copy(.7f))
+                    .padding(6.dp)
+            ) {
+                Text(
+                    text = movieList[itemIndex].title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .basicMarquee(),
+                    textAlign = TextAlign.Center,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(Modifier.align(Alignment.End)) {
+                    Icon(imageVector = Icons.Rounded.Star, contentDescription = "")
+                    Text(
+                        text = movieList[itemIndex].imdb_rating,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 8.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        maxLines = 2
+                    )
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/mada/movieapp/navigation/HomeScreen.kt
+++ app/src/main/java/com/mada/movieapp/navigation/HomeScreen.kt
@@ -1,9 +1,49 @@
 package com.mada.movieapp.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.mada.movieapp.item.ItemUi
+import com.mada.movieapp.item.TopBar
+import com.mada.movieapp.viewModel.MoviesViewModel
 
 @Composable
 fun HomeScreen(navController: NavController){
+    val movieViewModel = viewModel<MoviesViewModel>()
+    val state = movieViewModel.state
+    Scaffold(
+        modifier = Modifier.background(Color.Transparent),
+        topBar = {
+            TopBar()
+        }, content = { paddingValues ->
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+                    .background(
+                        Color.Transparent
+                    ),
+                content = {
+                    items(state.movies.size) {
+                        ItemUi(
+                        itemIndex = it, movieList = state.movies,
+                        navController = navController
+                    )
+                    }
+
+                }
+                    )
+
+                }
+            )
 
 }
